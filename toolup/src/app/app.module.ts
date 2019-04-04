51c8029ed86eef902076aca2b7ba46f42a0a658c
finn@work <finn@work>
2019-04-04 13:51:56 +0200

    Komponenten für die Seitenstruktur erstellt

--- toolup/src/app/app.module.ts
+++ toolup/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { FeaturesSearchComponent } from './components/features-search/features-search.component';
+import { FeatureSearchComponent } from './components/feature-search/feature-search.component';
+import { AppFeaturesComponent } from './components/app-features/app-features.component';
+import { AppsComponent } from './components/apps/apps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    CategoriesComponent,
+    FeaturesComponent,
+    FeaturesSearchComponent,
+    FeatureSearchComponent,
+    AppFeaturesComponent,
+    AppsComponent
   ],
   imports: [
     BrowserModule,
---
+++ toolup/src/app/components/feature-search/feature-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-search',
+  templateUrl: './feature-search.component.html',
+  styleUrls: ['./feature-search.component.scss']
+})
+export class FeatureSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
