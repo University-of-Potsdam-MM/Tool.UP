003a63177d3f4aac6376d4af2c146610b41f964f
finn@work <finn@work>
2019-04-09 14:08:59 +0200

    Component für das Formular erstellt. ref #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { FormComponent } from './components/form/form.component';
 
 
 
@@ -36,7 +37,8 @@ const appRoutes: Routes = [
     HomeComponent,
     HeaderComponent,
     LoginComponent,
-    CardsComponent
+    CardsComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
